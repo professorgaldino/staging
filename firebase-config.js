e5199931d8760d35d3eb13c0d3f857acbdcc77d0
professorgaldino <galdinoprofessor100@gmail.com>
2026-08-03 20:31:30 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIRlNEHZ0y_htrk16aL0Yu8__BiB_DPqQ",
+  authDomain: "villani-915de.firebaseapp.com",
+  projectId: "villani-915de",
+  storageBucket: "villani-915de.firebasestorage.app",
+  messagingSenderId: "1081762335556",
+  appId: "1:1081762335556:web:71bf78cfcbac8c79c9a511"
+};
